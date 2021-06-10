2a82566df334789b41e1d40787584413f3609da6
Klace Koch <koch.klace@gmail.com>
2021-06-10 02:25:10 -0700

    built dummy test for shift values being higher than 25

--- caesar_cipher/caesar_cipher.py
+++ caesar_cipher/caesar_cipher.py
@@ -8,30 +8,39 @@ def encrypt(text, shift):
   for char in text:
     # turns each char into unicode
     letter_unicode = ord(char)
+    # print(letter_unicode)
 
     # adds the shift value to each
     encrypted = letter_unicode + shift
-
+    # print(encrypted)
     # turns those new unicode values into new char adds chars to decrypted
     decrypted += chr(encrypted)
 
   #replaces the $ character with a plain ol space   
   decrypted = decrypted.replace("$"," ")
+
   return(decrypted)
 
-# dude = (encrypt('all cops are bastards',4))
-# print(dude)
 
-# if (dude == 'epp gstw evi fewxevhw'):
-#   print('oh yeah buddy')
+# def decrypt(text):
+
+# print(encrypt('all cops are bastards',4))
+# print(encrypt('butt sniffer',4))
+# print(encrypt('klace',9))
+
+
+##############################################################################
 
 if __name__ == '__main__':
 
   decrypted = encrypt('all cops are bastards', 4)
   assert decrypted == 'epp gstw evi fewxevhw'
 
-  # decrypted = encrypt('klace')
-  # assert decrypted == 'opegi'
+  decrypted = encrypt('klace',9)
+  assert decrypted == 'tujln'
+
+  decrypted = encrypt('abc',25)
+  assert (decrypted == 'zab')
 
 
-  print('TEST PASSED ')
+print('TESTS PASSED ')
