fe468047f6a039b5126546bbe9bbc5de2b5bdf28
Klace Koch <koch.klace@gmail.com>
2021-06-09 23:24:02 -0700

    encryption and decryption work - need to sort out what to do with the spaces

--- caesar_cipher/caesar_cipher.py
+++ caesar_cipher/caesar_cipher.py
@@ -1,1 +1,33 @@
 # print('ok im alive')
+
+def encrypt(text, shift):
+  # return 'epp gstw evi fewxevhw'
+  
+  decrypted = ""
+  
+  for char in text:
+    # turns each char into unicode
+    letter_unicode = ord(char)
+
+    # adds the shift value to each
+    encrypted = letter_unicode + shift
+
+    # turns those new unicode values into new char adds chars to decrypted
+    decrypted += chr(encrypted)
+
+  print('DECRYPTED OUTPUT: ' + decrypted)
+
+
+
+
+
+if __name__ == '__main__':
+
+  decrypted = encrypt('all cops are bastards', 4)
+  assert decrypted == 'epp gstw evi fewxevhw'
+
+  decrypted = encrypt('klace')
+  assert decrypted == 'opegi'
+
+
+  print('TEST PASSED ')
